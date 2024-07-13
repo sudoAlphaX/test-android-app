16c398a28051852b0d26199a6a6b3bdac3596a3f
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-07-13 14:52:18 +0530

    add padding, bgcolor, textcolor

--- app/src/main/java/com/example/test_android_app/MainActivity.kt
+++ app/src/main/java/com/example/test_android_app/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.test_android_app.ui.theme.TestandroidappTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan, contentColor = Color.Magenta) {
+        Text(
+            text = "Hello! I am $name",
+            modifier = modifier.padding(all = 5.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TestandroidappTheme {
-        Greeting("Android")
+        Greeting("Alpha")
     }
 }
